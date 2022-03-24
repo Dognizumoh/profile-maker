1b910c18367bc55b78f0615b972fc8a577d115ff
Dognizumoh <idafolo16@gmail.com>
2022-03-24 14:27:42 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import ProfileForm from './components/ProfileForm'
+import ProfileCard from './components/ProfileCard'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [allProfiles, setAllProfiles]=useState([
+{
+  firstname:"Ama",
+  lastname:"Mensah",
+  email:"mansah@gmail.com",
+    phone:"0208884444"
+
 }
 
-export default App;
+])
+const updateProfiles = (profile) =>{
+  let arr = allProfiles;
+  arr.push(profile);
+  setAllProfiles([...arr]);
+};
+
+  return (
+    <div className='app'>
+    <h1>Profile Maker</h1>
+    <div>
+    <ProfileForm submit={updateProfiles}/>
+    <hr/> 
+    <div className='list'>
+    {allProfiles.map((person,index)=>(
+      <ProfileCard key={index} card={person}/>
+    ))}
+    
+    </div> 
+    </div>
+    </div>
+
+  )
+}
+
+export default App
